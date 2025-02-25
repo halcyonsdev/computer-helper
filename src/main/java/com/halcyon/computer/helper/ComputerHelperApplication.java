122cc45229d558838ba934bd275f3d06717ca559
halcyonsdev <halcyon.ruslan@gmail.com>
2025-02-25 19:09:29 +0300

    first commit

---
+++ src/main/java/com/halcyon/computer/helper/ComputerHelperApplication.java
@@ -0,0 +1,11 @@
+package com.halcyon.computer.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerHelperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerHelperApplication.class, args);
+    }
+}
---
+++ src/main/java/com/halcyon/computer/helper/bot/ComputerHelperBot.java
@@ -0,0 +1,29 @@
+package com.halcyon.computer.helper.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class ComputerHelperBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @Override
+    public void consume(Update update) {
+
+    }
+}
